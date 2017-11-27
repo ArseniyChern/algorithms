7290d950712fbad948b6cbdba26cb38bfcacba62
Arseniy Chernykh <arseniychernykh@Arseniys-MacBook-Pro.local>
2017-11-28 00:03:53 +0200

    Added inversion sort explanation

--- Insertion Sort.cpp
+++ Insertion Sort.cpp
@@ -2,6 +2,37 @@
 
 using namespace std;
 
+/**
+
+Complexity = O(n + f(n)) where f(n) is the inversion count ~ O(n^2)
+
+[5,4,3]
+
+Here, inversion count is 3, so complexity will be n + 3, or 6. 
+
+For worst case scenario inversion count is equal to (n(n-1))/2, in this case that is (3(3-1))/2 = 3:
+
+Algorithm Steps:
+
+check 4 & 5 - 1
+
+swap 4 & 5 - 2
+[4,5,3]
+
+check 3 & 5 - 3
+
+swap 3 & 5 - 4
+[4,3,5]
+
+check 3 & 4 - 5
+
+swap 3 and 4 - 6
+[3,4,5]
+
+O(n + f(n)) = (3+3) = 6 ~ n^2 = 9
+
+**/
+
 void display(int* arr,int n) {
    int i;
    printf("[");
