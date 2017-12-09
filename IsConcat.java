cc25b1a238876e23deb1184d12ec82eefcb83b95
Arseniy Chernykh <arseniychernykh@Arseniys-MacBook-Pro.local>
2017-12-09 19:25:21 +0200

    added  aprogram ot check for string concat

--- IsConcat.java
+++ IsConcat.java
@@ -1,38 +1,75 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class IsConcat {
 
-	public static void main(String[] args) throws InterruptedException {
-		String[] lst = { "abc", "fgg", "dfg", "ert" };
-		String str = "agg";
+	public static void main(String[] args) {
+		String[] a = { "frt", "se", "sen", "sen", "sen", "senj", "fgt", "a", "tg", "r", "dfg", "dhf", "i", "y" };
+		boolean f = isConcatenation(a, "arseniysenj");
 
-		int currentCheckIndex = 0;
-		boolean back = false;
-		String currentCheckStr = "";
-		while (true) {
+		System.out.println(f);
+	}
 
-			boolean contains = false;
-			currentCheckStr = currentCheckStr + str.charAt(currentCheckIndex);
-			System.out.println(currentCheckStr);
-			Thread.sleep(1000);
-			for (String s : lst) {
-				if (s.contains(currentCheckStr)) {
-					currentCheckStr = "";
-					contains = true;
+	public static boolean isConcatenation(String[] s, String L) {
+
+		List<Integer> str = new ArrayList<Integer>();
+		String temp = null;
+		Integer checking = 2;
+		Integer tempIndex = null;
+
+		for (int j = 0; j < s.length; j++) {
+
+			for (int i = 0; i < s.length; i++) {
+				if (s[i].startsWith("" + L.charAt(0))) {
+					boolean isAMatch = true;
+
+					System.out.println("Possible Match: " + s[i]);
+
+					for (int i1 = 0; i1 < s[i].length(); i1++) {
+						if (L.length() == i1) {
+							isAMatch = false;
+							break;
+						}
+						if (s[i].charAt(i1) != L.charAt(i1)) {
+							isAMatch = false;
+							break;
+						}
+					}
+
+					if (isAMatch) {
+						if (temp != null && checking == 0) {
+							s[tempIndex] = temp;
+							temp = null;
+						}
+						checking--;
+						System.out.println("We found a match! It's " + s[i]);
+						str.add(i);
+						L = L.substring(s[i].length());
+						i = 0;
+						System.out.println("L is now " + L);
+						if (L.isEmpty()) {
+							return true;
+						}
+
+					}
 				}
-			}
-			if (currentCheckStr.equals("" + str.charAt(str.length() - 1)) || back == true) {
-				back = true;
-				currentCheckIndex--;
-			} else {
-				currentCheckIndex++;
-			}
-			if (currentCheckStr.equals(str)) {
-				System.out.println(contains);
-				break;
+
+				if (i == s.length - 1 && str.size() != 0) {
+					L = s[str.get(str.size() - 1)] + L;
+
+					temp = s[str.get(str.size() - 1)];
+					tempIndex = str.get(str.size() - 1);
+					checking = 2;
+					s[str.get(str.size() - 1)] = "";
+					str.remove(str.get(str.size() - 1));
+
+					i = 0;
+				}
 			}
 
 		}
 
+		return false;
 	}
 
 }
