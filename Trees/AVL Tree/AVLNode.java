415648c1a346f26bdc9fde8327c68677786307de
Arseniy Chernykh <arseniychernykh@Arseniys-MacBook-Pro.local>
2017-12-26 06:47:20 +0200

    fixed avl trees

--- Trees/AVL Tree/AVLNode.java
+++ Trees/AVL Tree/AVLNode.java
@@ -1,9 +1,17 @@
+package Trees.AVL;
 
 public class AVLNode {
+	
+	@Override
+	public String toString() {
+		return ""+Data;
+	}
 
 	public int Data;
 	
 	public int Delta;
+	
+	public int Length;
 	
 	
 	public AVLNode Parent;
